abda92014439e0ba2bd011ee77e282b96404e3d3
Sharkevich Igor <sharkevich.i@inbox.ru>
2022-03-21 20:18:08 +0300

    add some space

--- index.js
+++ index.js
@@ -37,29 +37,40 @@ const lessThanOrEqual = (num1, num2) => {
 const equal = (num1, num2) => {
     return num1 === num2;
 }
+
 const notEqual = (num1, num2) => {
     return num1 !== num2;
 }
 
 console.log('Сумма чисел равна:')
 console.log(sum(3, 4))
+
 console.log('Разность чисел равна:')
 console.log(sub(7,2))
+
 console.log('Произведение чисел равно:')
 console.log(multiply(7,2))
+
 console.log('Частное чисел равно:')
 console.log(div(7,2))
+
 console.log('Остаток от деления чисел равен:')
 console.log(modulo(14,3))
+
 console.log('Первое число больше второго:')
 console.log(moreThan(14,3))
+
 console.log('Первое число меньше второго:')
 console.log(lessThan(1,3))
+
 console.log('Первое число больше либо равно второму:')
 console.log(moreThanOrEqual(2,3))
+
 console.log('Первое число меньше либо равно второму:')
 console.log(lessThanOrEqual(1,3))
+
 console.log('Первое число равно второму:')
 console.log(equal(4,4))
+
 console.log('Первое число не равно второму:')
 console.log(notEqual(4,4))
